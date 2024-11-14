d083b386156730c5409c7d597a25929a04faa68b
development-hobby <jesusalcalarojas@gmail.com>
2024-11-14 09:28:20 -0400

    chore: builds config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#121212',
+        primary: '#F9D423',
+        secondary: {
+          100: '#C380F7',
+          200: '#B96BF6',
+          300: '#9B2BF2',
+        },
+      },
+    },
+  },
+  plugins: [],
 }
